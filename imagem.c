f6df28da4786b35ab6b8872e2100574c1ff4d726
gcontiero11 <gustcontiero@gmail.com>
2024-03-03 20:25:04 -0300

    lerArquivo aprimorado

--- imagem.c
+++ imagem.c
@@ -5,70 +5,87 @@
 typedef struct {
     char* nome;
     int numeroColunas, numeroLinhas, maiorPixel;
-    char** matriz;
+    int** matriz;
 } Arquivo;
 
-char** criaMatrizDinamica(int numeroLinhas, int numeroColunas){
-    char** matriz = (char**) malloc(sizeof(char**) * numeroLinhas + 1);
+int** criaMatrizDinamica(int numeroLinhas, int numeroColunas){
+    int** matriz;
+    matriz = (int**) malloc(sizeof(int*) * numeroLinhas);
     for (int i = 0; i < numeroLinhas; i++){
-        matriz[i] = (char*) malloc(sizeof(char*) * numeroColunas + 1);
+        matriz[i] = malloc(sizeof(int) * numeroColunas);
     }
     return matriz;
 }
 
-char* escreverArquivo(Arquivo *arquivoEntrada, FILE *arquivoSaida){
-    arquivo = ("saida.pgmc", "w");
+// char* escreverArquivo(Arquivo *arquivoEntrada, FILE *arquivoSaida){
+//     arquivo = ("saida.pgmc", "w");
 
-    fprintf(arquivo, )
+//     fprintf(arquivo, );
     
     
-    fprintf("%d %d", arquivoEntrada->numeroColunas, arquivoEntrada->numeroLinhas);
+//     fprintf("%d %d", arquivoEntrada->numeroColunas, arquivoEntrada->numeroLinhas);
     
-}
+// }
 
-char* descompactarArquivo(Arquivo *arquivoEntrada, FILE *arquivoSaida){
-    arquivo = ("saida.pgm", "w");
+// char* descompactarArquivo(Arquivo *arquivoEntrada, FILE *arquivoSaida){
+//     arquivo = ("saida.pgm", "w");
 
-    char **novaMatriz = criaMatrizDinamica(arquivoEntrada->numeroLinhas, arquivoEntrada->numeroColunas);
+//     char **novaMatriz = criaMatrizDinamica(arquivoEntrada->numeroLinhas, arquivoEntrada->numeroColunas);
     
-    for(int i = 0; i < arquivoEntrada->numeroLinhas; i++){
-        for(int j = 0; j < arquivoEntrada->numeroColunas; j++){
-            if(arquivoEntrada->matriz[i][j] == "@"){
-                for(int k = 0; k < arquivoEntrada->matriz[i][j+4]){
-                    novaMatriz[i][j + k] = arquivoEntrada->matriz[i][j+2];
-                }
-            } else {
-                novaMatriz[i][j] = arquivoEntrada->matriz[i][j];
-            }
+//     for(int i = 0; i < arquivoEntrada->numeroLinhas; i++){
+//         for(int j = 0; j < arquivoEntrada->numeroColunas; j++){
+//             if(arquivoEntrada->matriz[i][j] == "@"){
+//                 for(int k = 0; k < arquivoEntrada->matriz[i][j+4]){
+//                     novaMatriz[i][j + k] = arquivoEntrada->matriz[i][j+2];
+//                 }
+//             } else {
+//                 novaMatriz[i][j] = arquivoEntrada->matriz[i][j];
+//             }
+//         }
+//     }
+
+//     return novaMatriz;
+
+// }
+
+void printMatriz(int** matriz, int numeroLinhas , int numeroColunas){
+    for (int i = 0; i < numeroLinhas; i++)
+    {
+        for (int j = 0; j < numeroColunas; j++)
+        {
+            printf("%d ",matriz[i][j]);
         }
+        printf("\n");
     }
-
-    return novaMatriz;
-
+    
 }
 
 Arquivo* lerArquivo(FILE* arquivo){
-    printf("passou");
     Arquivo *novoArquivo = (Arquivo*) malloc(sizeof(Arquivo) * 1);
 
     fscanf(arquivo,"%s", novoArquivo->nome);
     fscanf(arquivo, "%d %d", &novoArquivo->numeroColunas, &novoArquivo->numeroLinhas);
-    fscanf(arquivo, "%d", &novoArquivo->maiorPixel);
-
+    fscanf(arquivo, "%d", &novoArquivo->maiorPixel); 
+    printf("%d\n",novoArquivo->numeroLinhas);
     novoArquivo->matriz = criaMatrizDinamica(novoArquivo->numeroLinhas, novoArquivo->numeroColunas);
     
     //Preenche Matriz;
+    char prototipo;
     for (int i = 0; i < novoArquivo->numeroLinhas; i++){
         for (int j = 0; j < novoArquivo->numeroColunas; j++)
-        {
-            fscanf(arquivo,"%c", &novoArquivo->matriz[i][j]);
+        {   
+            fscanf(arquivo,"%c", &prototipo);
+            if (prototipo != ' ')
+            {
+                novoArquivo->matriz[i][j] = prototipo;
+            }
         }
         
     }
     
     printf("%s\n", novoArquivo->nome);
     printf("%d %d\n",novoArquivo->numeroColunas,novoArquivo->numeroLinhas); // teste
-    // printMatriz(novoArquivo->matriz,novoArquivo->numeroLinhas,novoArquivo->numeroColunas);
+    printMatriz(novoArquivo->matriz,novoArquivo->numeroLinhas,novoArquivo->numeroColunas);
     
     // if(novoArquivo->nome == "P2")Compactar(arquivo);
     // else Descompactar(arquivo);
@@ -79,12 +96,12 @@ Arquivo* lerArquivo(FILE* arquivo){
     return novoArquivo;
         
 }
-
-int main(int argc, char *argv[]){
-    if (argc != 3) {
-        printf("Uso: %s arquivo_entrada arquivo_saida\n", argv[0]);
-        return 1;
-    }
+// int argc, char *argv[]
+int main(){
+    // if (argc != 3) {
+    //     printf("Uso: %s arquivo_entrada arquivo_saida\n", argv[0]);
+    //     return 1;
+    // }
 
     FILE* arquivoEntrada;
     FILE* arquivoSaida;
--- testeimg.c
+++ testeimg.c
@@ -3,42 +3,204 @@
 #include <string.h>
 
 typedef struct {
-    char tipo[3];
-    int colunas, linhas, maiorPixel;
-    char** matriz;
-} Imagem;
+    char nome[10];
+    int numeroColunas, numeroLinhas, maiorPixel;
+    int** matriz;
+} Arquivo;
 
-Imagem lerArquivo(FILE *arquivo){
-    Imagem imagem;
+// char lerArquivoCompactado(FILE arquivo*){
+//     imagem = fopen("entrada.pgm", "r");
 
-    fscanf(arquivo, "%s %d %d %d", imagem.tipo, &imagem.colunas, &imagem.linhas, &imagem.maiorPixel);
+//     if (imagem == null) {
+//         print("Erro na leitura do arquivo");
+//         return;
+//     }
 
-    printf("%s \n", imagem.tipo);
-    printf("%d %d \n", imagem.colunas, imagem.linhas);
-    printf("%d", imagem.maiorPixel);
+//     char linha[tamanhoLinha];
+//     char *leitura;
+//     leitura = fgets(linha, 24, imagem); 
+    
+//     descompactarArquivo(leitura);
+// }
+
+// char lerArquivoDescompactado(FILE arquivo*){
+//         FILE *imagem;
+
+//         imagem = fopen("entrada.pgmc", "r");
+
+//         if (imagem == null) {
+//             print("Erro na leitura do arquivo");
+//             return;
+//     }
+
+//     char* linha;
+//     linha = char* malloc(sizeof(char) * tamanhoLinha);
+//     char *leitura;
+//     leitura = fgets(linha, tamanhoMatriz, imagem);
+
+//     compactarArquivo(linha);
+// }
+
+// char compactarLinha(char *linha, char arqSaida, int *tamanhoLinha){
+//     arqSaida = fopen(arqSaida, "w");
+//     int conta_repeticao = 0;
+//     int marcador = arqSaida[0];
+//     int inicio = arqSaida[0];
+//     for (int i = 0; i < tamanhoLinha; i++){
+//         if(arqSaida[i] == marcador){
+//             conta_repeticao += 1;
+//             if(conta_repeticao > 3){
+//             inicio = (char) "@";
+//             arqSaida[inicio + 1] = marcador;
+//             arqSaida[inicio + 2] = conta_repeticao;
+//             } 
+//         }
+//         else{
+//             conta_repeticao = 0
+//             marcador = arqSaida[i];
+//             inicio = arqSaida[i];
+//         }
+//     }
+//     return arqSaida;
+// }
 
 
 
-    return imagem;
+// char descompactarLinha(char *linha){
+    
+// }
+void printMatriz(int** matriz,int numeroLinha,int numeroColuna){
+    for (size_t i = 0; i < numeroLinha; i++){
+        for (size_t j = 0; j < numeroColuna; j++){
+            printf("%d ",matriz[i][j]);
+        }
+        printf("\n");
+    }
+    
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Uso: %s arquivo_entrada arquivo_saida\n", argv[0]);
-        return 1;
+
+int** criaMatrizDinamica(int numeroLinhas,int numeroColunas){
+    int** matriz;
+    matriz = (int**) malloc(sizeof(int*) * numeroLinhas);
+    for (int i = 0; i < numeroLinhas; i++){
+        matriz[i] = malloc(sizeof(int) * numeroColunas);
     }
-
-    FILE *arquivoEntrada = fopen("exemplo1.pgm", "r");
-
-    if (arquivoEntrada == NULL) {
-        perror("Erro ao abrir o arquivo");
-        return 0;
-    }
-
-    lerArquivo(arquivoEntrada);
-    
-    
-    
-    fclose(arquivoEntrada);
-    return 0;
+    return matriz;
 }
+
+void preencheMatrizDesconpactada(FILE* arquivo,Arquivo* novoArquivo){
+    for (int i = 0; i < novoArquivo->numeroLinhas; i++){
+        for (int j = 0; j < novoArquivo->numeroColunas; j++){   
+            fscanf(arquivo,"%d",&novoArquivo->matriz[i][j]);
+        }
+    }
+}
+
+int** preencheMatrizCompactada(FILE* arquivo, Arquivo* novoArquivo){
+    for (int i = 0; i < novoArquivo->numeroLinhas; i++){
+        char prototipo;
+        for (int j = 0; j < novoArquivo->numeroColunas; j++){ 
+
+            fscanf(arquivo,"%c",&prototipo);
+            if (prototipo == '@')
+            {
+                novoArquivo->matriz[i][j] =  prototipo;
+            }
+    
+            fscanf(arquivo,"%d",&novoArquivo->matriz[i][j]);
+        }
+    }
+}
+
+FILE* abrirArquivo(char* nomeArquivo){
+    FILE* arquivo;
+    arquivo = fopen(nomeArquivo, "r");
+    return arquivo;
+}
+
+Arquivo* lerArquivo(FILE* arquivo){
+    if(arquivo == NULL) printf("Erro na leitura");
+    
+    Arquivo *novoArquivo = (Arquivo*) malloc(sizeof(Arquivo) * 1);
+    fscanf(arquivo,"%s", &novoArquivo->nome);
+    printf("%s\n",novoArquivo->nome);
+    fscanf(arquivo, "%d %d", &novoArquivo->numeroColunas, &novoArquivo->numeroLinhas);
+    fscanf(arquivo, "%d", &novoArquivo->maiorPixel);
+
+    novoArquivo->matriz = criaMatrizDinamica(novoArquivo->numeroLinhas,novoArquivo->numeroColunas);
+    if (strcmp(novoArquivo->nome,"P2") == 0){  
+        printf("DEU BOM!\n");
+        preencheMatrizDesconpactada(arquivo,novoArquivo);
+    }
+    else{
+        printf("DEU RUIM!\n");
+    }
+    // else{
+    //     novoArquivo->matriz = preencheMatrizCompactada(novoArquivo);
+    // }
+    
+    //Preenche Matriz;
+    
+    
+    // printf("%s\n",&novoArquivo->nome);
+    // printf("%d %d\n",novoArquivo->numeroColunas,novoArquivo->numeroLinhas); // teste
+    printMatriz(novoArquivo->matriz,novoArquivo->numeroLinhas,novoArquivo->numeroColunas);
+
+    
+    // if(novoArquivo->nome == "P2")Compactar(arquivo);
+    // else Descompactar(arquivo);
+    return novoArquivo;
+        
+}
+
+int main(){
+    FILE* arquivo;
+    arquivo = abrirArquivo("exemplo1.pgm");
+    Arquivo *novoArquivo;
+    novoArquivo = lerArquivo(arquivo);
+    // *novoArquivo = transformaArquivo(novoArquivo);
+    // escreveArquivo(novoArquivo);
+}
+
+// Arquivo transformaArquivo(Arquivo arquivo){
+//     int numeroLinhas = arquivo.numeroLinhas;
+//     Arquivo *novoArquivo;
+//     *novoArquivo = lerArquivo();
+    
+//     for(int i = 0; i<numeroLinhas;i++){
+        
+//     }
+    
+// }
+
+
+
+
+
+/* int matriz[linhas][colunas];
+int conta_repeticao = 0;
+int marcador = matriz[0][0];
+for (i = 0; i < linhas; i++){
+conta_repeticao = 0;
+int inicio = matriz[i][j]; 
+int num_repetido = matriz[i+1] []]; 
+int quantidade = matriz[i+2][j]; 
+
+for (j = 0; j < colunas; i++){
+    if (matriz[i][j] == marcador){
+        conta_repeticao += 1; 
+
+        if (conta_repeticao > 3){ 
+            inicio = (char) "@"; 
+            num_repetido = marcador; 
+            quantidade = conta_repeticao;
+        }
+
+    else{
+        conta_repeticao = 0;
+        marcador = matriz[i][j];
+} */
+
+// if (valor[i] == "@")
+
